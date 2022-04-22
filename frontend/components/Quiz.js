27af272b53f29e44e2b95854ba877d0207e00ee7
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-04-22 09:50:24 -0600

    successful post answer

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import * as actions from '../state/action-creators'
 
 export function Quiz(props) {
-const {quiz, selectAnswer, fetchQuiz, selectedAnswer } = props
+const {quiz, selectAnswer, fetchQuiz, selectedAnswer, postAnswer } = props
 console.log(props)
 
 useEffect(() => {
@@ -19,22 +19,22 @@ useEffect(() => {
             <h2>{quiz.question}</h2>
 
             <div id="quizAnswers">
-              <div className={selectedAnswer === 1 ? 'answer selected' : 'answer'}>
+              <div className={selectedAnswer === quiz.answer1Id ? 'answer selected' : 'answer'}>
                 {quiz.answer1}
-                <button onClick={() => selectAnswer(1)}>
-                {selectedAnswer === 1 ? 'SELECTED' : 'select'}
+                <button onClick={() => selectAnswer(quiz.answer1Id)}>
+                {selectedAnswer === quiz.answer1Id ? 'SELECTED' : 'select'}
                 </button>
               </div>
 
-              <div className={selectedAnswer === 2 ? 'answer selected' : 'answer'}>
+              <div className={selectedAnswer === quiz.answer2Id ? 'answer selected' : 'answer'}>
               {quiz.answer2}
-              <button onClick={() => selectAnswer(2)}>
-              {selectedAnswer === 2 ? 'SELECTED' : 'select'}
+              <button onClick={() => selectAnswer(quiz.answer2Id)}>
+              {selectedAnswer === quiz.answer2Id ? 'SELECTED' : 'select'}
                 </button>
               </div>
             </div>
 
-            <button id="submitAnswerBtn" disabled={selectedAnswer ? false : true}>Submit answer</button>
+            <button id="submitAnswerBtn" disabled={selectedAnswer ? false : true} onClick={() => postAnswer(quiz.quizId, selectedAnswer)}>Submit answer</button>
           </>
         ) : 'Loading next quiz...'
       }
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -14,10 +14,10 @@ export function moveCounterClockwise() {
   }
 }
 
-export function selectAnswer(num) { 
+export function selectAnswer(id) { 
   return {
     type: types.SET_SELECTED_ANSWER,
-    payload: num
+    payload: id
   }
 }
 
@@ -42,20 +42,34 @@ export function fetchQuiz() {
     .then(res => {
       const question = res.data.question
       const answer1 = res.data.answers[0].text
+      const answer1Id = res.data.answers[0].answer_id
       const answer2 = res.data.answers[1].text
-      dispatch({type: types.SET_QUIZ_INTO_STATE, payload: {question, answer1, answer2}})
+      const answer2Id = res.data.answers[1].answer_id
+      const quizId = res.data.quiz_id
+      dispatch({type: types.SET_QUIZ_INTO_STATE, payload: {question, answer1, answer2, quizId, answer1Id, answer2Id}})
     })
     .catch(err => {
       debugger
     })
   }
 }
-export function postAnswer() {
+export function postAnswer(quizId, answerId) {
   return function (dispatch) {
+    const payload = {
+       "quiz_id": `${quizId}`, "answer_id": `${answerId}`
+    }
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
+    axios.post('http://localhost:9000/api/quiz/answer', payload)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        debugger
+      })
+
   }
 }
 export function postQuiz() {
